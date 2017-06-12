0f2c965308a06462aeb19d1e284306d6848d4aea
Pablo Saavedra <psaavedra@igalia.com>
2017-06-12 16:53:47 +0200

    New tickets are also notified

--- matrixbot/plugins/trac.py
+++ matrixbot/plugins/trac.py
@@ -1,4 +1,4 @@
-import xmlrpclib 
+import xmlrpclib
 from datetime import datetime, timedelta
 from matrixbot import utils
 
@@ -10,7 +10,7 @@ class TracPlugin:
         self.timestamp = datetime.utcnow()
         self.server = xmlrpclib.ServerProxy(
             '%(url_protocol)s://%(url_auth_user)s:%(url_auth_password)s@%(url_domain)s%(url_path)s/login/xmlrpc' % self.settings
-        ) 
+        )
 
     def pretty_ticket(self, ticket):
         ticket[3]["ticket_id"] = ticket[0]
@@ -24,15 +24,18 @@ class TracPlugin:
     def async(self, handler):
         self.logger.info("TracPlugin async")
         server = self.server
-        multicall = xmlrpclib.MultiCall(server) 
+        multicall = xmlrpclib.MultiCall(server)
 
         d = self.timestamp
         self.timestamp = datetime.utcnow()
         res = []
-        for t in server.ticket.getRecentChanges(d): 
+        for t in server.ticket.getRecentChanges(d):
             # self.logger.info(t)
             ticket = server.ticket.get(t)
-            for c in server.ticket.changeLog(t):
+            changes = server.ticket.changeLog(t)
+            if len(changes) == 0 and 'new' in self.settings['status']:  # No changes implies New ticket
+                res.append(ticket)
+            for c in changes:
                 # self.logger.info(c)
                 if (
                     c[0] > d and c[2] == 'status'
@@ -50,7 +53,7 @@ class TracPlugin:
         message = "\n".join(res)
         for room_id in self.settings["rooms"]:
             handler(room_id, message)
- 
+
     def command(self, sender, room_id, body, handler):
         self.logger.info("TracPlugin command")
         return ""
